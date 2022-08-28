56b50adc9157e07d57a5ea6d4b36d0cf627bacdf
onaroll2021 <lukeli.onaroll@gmail.com>
2022-08-28 07:29:25 +0000

    yeah  all done

---
+++ function-arguments.js
@@ -0,0 +1,6 @@
+function math (neo, nini, nori){
+  let i ;
+  i = neo + nini * nori;
+  return i
+}
+console.log(math(53, 61, 67))
---
+++ fundamentals/tinkering.js
@@ -0,0 +1,7 @@
+function multiple  (a,b){
+  return a * b
+}
+const  result = multiple (2,4);
+console.log(result)
+
+3
